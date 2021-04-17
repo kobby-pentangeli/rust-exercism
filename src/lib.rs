083476982bb2653f01e0650492eb21955e0fb8f1
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-04-17 09:10:55 +0000

    feat: Initialize the basic functionality of the library

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+//! *** Rust-Exercism - My solutions to the exercises in the Rust track of Exercism ***
+
+#![forbid(
+    arithmetic_overflow,
+    mutable_transmutes,
+    no_mangle_const_items,
+    unknown_crate_types
+)]
+#![warn(
+    bad_style,
+    deprecated,
+    improper_ctypes,
+    missing_docs,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    stable_features,
+    unconditional_recursion,
+    unknown_lints,
+    unsafe_code,
+    unused,
+    unused_allocation,
+    unused_attributes,
+    unused_comparisons,
+    unused_features,
+    unused_parens,
+    while_true,
+    clippy::unicode_not_nfc,
+    clippy::wrong_pub_self_convention,
+    clippy::unwrap_used,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
